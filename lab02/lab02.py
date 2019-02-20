a59deaa72a7d95881f7b8e1171817a6f183db24b
Zach Baklund <baklund.zachary@uwlax.edu>
2019-02-20 13:59:15 -0600

    lab02 --finished

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -1,2 +1,19 @@
 from os import listdir
 
+contains = [f for f in [rs for rs in listdir('datafiles')] 
+                if f+".rand" in listdir('datafiles/'+f)]
+print contains
+print
+
+evens = [f for f in [rs for rs in listdir('datafiles')] 
+            if int(f.split('.')[1]) % 2 == 0]
+print evens
+print
+
+pairs = [(f1, f2) for f1 in [rs for rs in listdir('datafiles')]
+            for f2 in [rs for rs in listdir('datafiles')] 
+            if (int(f1.split('.')[1]) + int(f2.split('.')[1]))% 100 == 0]
+print pairs
+print
+
+
